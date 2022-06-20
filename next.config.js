5380c07b701e6b62f6bf0c57ba430a9b93743136
Kamil Szerląg <kamilszerlag@gmail.com>
2022-06-20 20:23:01 +0200

    initial files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  trailingSlash: true, //Wymuszamy, aby każdy URL w aplikacji kończył się znakiem / (tj. trailing slash) // https://nextjs.org/docs/api-reference/next.config.js/trailing-slash
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+//_document.tsx generowany jest tylko raz.
+
+export default function Document() {
+  return (
+    <Html lang="pl">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
