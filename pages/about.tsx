8215282efe749ffeff45c442afaff26a156986be
Kamil Szerląg <kamilszerlag@gmail.com>
2022-06-23 20:25:46 +0200

    initial files 2

---
+++ pages/about.tsx
@@ -0,0 +1,5 @@
+const AboutPage = () => {
+  return <div>About Page</div>;
+};
+
+export default AboutPage; //tylko export default działa w pages, bo tego wymaga Next.js
---
+++ pages/users.tsx
@@ -0,0 +1,5 @@
+const UsersPage2 = () => {
+  return <div>Users Page 2</div>;
+};
+
+export default UsersPage2;
